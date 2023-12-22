605bc8d33936417d152c8db9fa40c6447bb45862
APalaciosQ <93203495+APalaciosQ@users.noreply.github.com>
2023-12-21 23:31:04 -0400

    Completada la implementacion del Bloc Artist Detail

--- lib/application/BLoC/artist_detail/artist_detail_bloc.dart
+++ lib/application/BLoC/artist_detail/artist_detail_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
+import 'package:sign_in_bloc/application/use_cases/album/get_albums_by_artist_use_case.dart';
 import 'package:sign_in_bloc/application/use_cases/artist/get_artist_data_use_case.dart';
 import 'package:equatable/equatable.dart';
+import 'package:sign_in_bloc/application/use_cases/song/get_songs_by_album_use_case.dart';
 import 'package:sign_in_bloc/domain/artist/artist.dart';
 import 'package:sign_in_bloc/domain/album/album.dart';
 import '../../../common/failure.dart';
@@ -10,8 +12,13 @@ part 'artist_detail_state.dart';
 
 class ArtistDetailBloc extends Bloc<ArtistDetailEvent, ArtistDetailState> {
   final GetArtistDataUseCase getArtistDataUseCase;
+  final GetAlbumsByArtistUseCase getAlbumsByArtistUseCase;
+  final GetSongsByAlbumUseCase getSongsByAlbumUseCase;
 
-  ArtistDetailBloc({required this.getArtistDataUseCase})
+  ArtistDetailBloc(
+      {required this.getArtistDataUseCase,
+      required this.getAlbumsByArtistUseCase,
+      required this.getSongsByAlbumUseCase})
       : super(ArtistDetailLoading()) {
     on<FetchArtistDetailEvent>(_fetchArtistDetailsEventHandler);
   }
@@ -19,8 +26,8 @@ class ArtistDetailBloc extends Bloc<ArtistDetailEvent, ArtistDetailState> {
   void _fetchArtistDetailsEventHandler(
       FetchArtistDetailEvent event, Emitter<ArtistDetailState> emit) async {
     final artistDataResult = await getArtistDataUseCase.execute();
-    final albumsByArtistResult = await getPromotionalBannerUseCase.execute();
-    final songByAlbumResult = await getTrendingAlbumsUseCase.execute();
+    final albumsByArtistResult = await getAlbumsByArtistUseCase.execute();
+    final songByAlbumResult = await getSongsByAlbumUseCase.execute();
 
     if ([artistDataResult, albumsByArtistResult, songByAlbumResult]
         .every((result) => result.hasValue())) {
---
+++ lib/application/BLoC/artist_detail/artist_detail_event.dart
@@ -0,0 +1,5 @@
+part of 'artist_detail_bloc.dart';
+
+abstract class ArtistDetailEvent {}
+
+class FetchArtistDetailEvent extends ArtistDetailEvent {}
